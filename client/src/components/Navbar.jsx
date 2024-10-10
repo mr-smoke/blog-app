e7fc70a642b81a251b65ef0d9348c1129ea0d87a
baki duman <g.s_baki_70@hotmail.com>
2024-10-10 20:38:05 +0300

    Navbar and Home components added.

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,123 @@
+import { useState } from "react";
+import { FaBloggerB } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import { MdClose, MdDehaze } from "react-icons/md";
+
+const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+  const currentUser = "name";
+
+  return (
+    <nav className="flex items-center justify-between py-3 font-mono text-2xl px-3 lg:px-0">
+      <div className="before:block before:absolute before:-inset-1 before:-skew-y-3 before:bg-violet-700 relative inline-block">
+        <Link to="/">
+          <h1 className="flex items-center gap-1 text-3xl relative text-white">
+            <FaBloggerB className="relative" />
+            Blogger
+          </h1>
+        </Link>
+      </div>
+      <div className="md:hidden" onClick={() => setMenuOpen(!menuOpen)}>
+        <MdDehaze size={30} />
+      </div>
+      <div
+        className={`${
+          menuOpen ? "translate-x-0" : "w-0 invisible p-0 md:visible"
+        } flex gap-5 md:relative md:flex-row md:p-0 md:items-start md:h-auto md:w-auto md:bg-white md:text-black md:translate-x-0 fixed top-0 right-0 flex-col items-end p-3 h-screen w-1/2 bg-violet-700 text-white translate-x-full transition-all z-20`}
+      >
+        <div className="md:hidden" onClick={() => setMenuOpen(!menuOpen)}>
+          <MdClose size={30} />
+        </div>
+        <Link to="/art">
+          <p className="before:absolute before:pointer-events-none before:w-full before:h-0.5 before:-bottom-1 before:left-0 before:bg-violet-700 before:translate-x-[-100%] before:opacity-0 relative before:transition-all before:duration-300 hover:before:translate-x-0 hover:before:opacity-100">
+            Art
+          </p>
+        </Link>
+        <Link to="/science">
+          <p className="before:absolute before:pointer-events-none before:w-full before:h-0.5 before:-bottom-1 before:left-0 before:bg-violet-700 before:translate-x-[-100%] before:opacity-0 relative before:transition-all before:duration-500 hover:before:translate-x-0 hover:before:opacity-100">
+            Science
+          </p>
+        </Link>
+        <Link to="/write">
+          <p className="flex md:hidden bg-white rounded-full w-20 h-20 items-center justify-center text-violet-700">
+            Write
+          </p>
+        </Link>
+      </div>
+      <div className="md:flex md:text-2xl md:order-none items-center gap-5 font-semibold text-xl -order-1">
+        <span className="flex flex-col items-center gap-1">
+          {currentUser ? (
+            <>
+              <img
+                src="https://randomuser.me/api/port"
+                alt="profile"
+                className="w-10 h-10 rounded-full"
+              />
+              <Link to="/logout">
+                <p>Logout</p>
+              </Link>
+            </>
+          ) : (
+            <Link to="/login">
+              <p>Login</p>
+            </Link>
+          )}
+        </span>
+        <Link className="" to="/write">
+          <p className="hidden md:flex bg-violet-700 rounded-full w-20 h-20 items-center justify-center text-white hover:bg-white hover:text-violet-700 hover:border">
+            Write
+          </p>
+        </Link>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
+
+{
+  /* <nav className="flex items-center justify-between py-3 font-mono text-2xl px-3 lg:px-0">
+  <Link to="/">
+    <span className="before:block before:absolute before:-inset-1 before:-skew-y-3 before:bg-violet-700 relative inline-block">
+      <h1 className="flex items-center gap-1 text-3xl relative text-white">
+        <FaBloggerB className="relative" />
+        Blogger
+      </h1>
+    </span>
+  </Link>
+  <div className="flex flex-col w-44 md:w-auto h-screen md:h-auto md:flex-row items-end md:items-center md:flex-1 fixed md:relative top-0 right-0 flex-none bg-white z-10 gap-8 p-3 md:p-0">
+    <div className="md:hidden">
+      <MdDehaze size={30} />
+    </div>
+    <div className="flex flex-col md:flex-row flex-none md:flex-1 justify-center gap-5 items-end">
+      <Link to="/art">
+        <p className="before:absolute before:pointer-events-none before:w-full before:h-0.5 before:-bottom-1 before:left-0 before:bg-violet-700 before:translate-x-[-100%] before:opacity-0 relative before:transition-all before:duration-300 hover:before:translate-x-0 hover:before:opacity-100">
+          Art
+        </p>
+      </Link>
+      <Link to="/science">
+        <p className="before:absolute before:pointer-events-none before:w-full before:h-0.5 before:-bottom-1 before:left-0 before:bg-violet-700 before:translate-x-[-100%] before:opacity-0 relative before:transition-all before:duration-500 hover:before:translate-x-0 hover:before:opacity-100">
+          Science
+        </p>
+      </Link>
+    </div>
+    <div className="flex flex-col md:flex-row items-center gap-5 md:font-semibold">
+      <span>{currentUser}</span>
+      {currentUser ? (
+        <Link to="/logout">
+          <p>Logout</p>
+        </Link>
+      ) : (
+        <Link to="/login">
+          <p>Login</p>
+        </Link>
+      )}
+      <Link to="/write">
+        <p className="bg-violet-700 rounded-full w-20 h-20 flex items-center justify-center text-white hover:bg-white hover:text-violet-700 hover:border">
+          Write
+        </p>
+      </Link>
+    </div>
+  </div>
+  </nav> */
+}
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,39 @@
+import Navbar from "../components/Navbar";
+
+const Home = () => {
+  return (
+    <div className="max-w-[1040px] mx-auto">
+      <Navbar />
+      <main className="flex flex-col">
+        <section className="flex flex-col md:flex-row pb-16 md:py-16">
+          <div className="flex flex-col gap-8 md:w-2/3 p-3 md:pr-16">
+            <h2 className="text-5xl leading-tight font-bold">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit
+            </h2>
+            <p className="text-lg">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+              quia, voluptatem, voluptas, quos asperiores cumque quae nemo
+              doloremque voluptatibus quidem tempora. Quisquam quia, voluptatem,
+              voluptas, quos asperiores cumque quae nemo doloremque voluptatibus
+              quidem tempora.
+            </p>
+            <button className="bg-violet-700 text-white px-5 py-2 w-max hover:bg-white hover:text-violet-700 hover:border">
+              Read More
+            </button>
+          </div>
+          <div className="md:w-1/3 p-3">
+            <span className="relative after:absolute after:top-5 after:-left-5 after:w-full after:h-full after:z-[-1] after:bg-violet-700">
+              <img
+                className="w-full max-h-[400px] object-cover "
+                src="https://images.pexels.com/photos/28762715/pexels-photo-28762715/free-photo-of-muhtesem-kumtasi-antilop-kanyonu.jpeg"
+                alt="random"
+              />
+            </span>
+          </div>
+        </section>
+      </main>
+    </div>
+  );
+};
+
+export default Home;
