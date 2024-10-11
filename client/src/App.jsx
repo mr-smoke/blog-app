4397d2c379e2592e3ae8d0efc4957487de583636
baki duman <g.s_baki_70@hotmail.com>
2024-10-11 10:15:25 +0300

    Login page added.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Blog from "./pages/Blog";
 import AddPost from "./pages/AddPost";
+import Login from "./pages/Login";
 
 function App() {
   return (
@@ -9,7 +10,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/blog/:id" element={<Blog />} />
-        <Route path="add" element={<AddPost />} />
+        <Route path="/add" element={<AddPost />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </>
   );
---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,48 @@
+const Login = () => {
+  return (
+    <main className="flex items-center justify-center h-screen bg">
+      <div className="border rounded-xl flex flex-col gap-3 py-16 p-8 bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-0 text-white xl:w-1/4">
+        <h1 className="text-3xl text-center">
+          Login <span className="text-violet-700 font-bold">Blogger</span>
+        </h1>
+        <form className="flex flex-col gap-4">
+          <div className="flex flex-col gap-1">
+            <label htmlFor="username">Username</label>
+            <input
+              className="border rounded-lg p-3 text-black"
+              type="text"
+              id="username"
+              maxLength={20}
+              onChange={(e) => setForm({ ...form, email: e.target.value })}
+            />
+          </div>
+          <div className="flex flex-col gap-1">
+            <label htmlFor="password">Password</label>
+            <input
+              className="border rounded-lg p-3 text-black"
+              type="password"
+              id="password"
+              onChange={(e) => setForm({ ...form, password: e.target.value })}
+            />
+          </div>
+          <p>
+            Don't have an account?{" "}
+            <a href="/signup" className="text-blue-500">
+              Signup
+            </a>
+          </p>
+          <div className="flex justify-center">
+            <button
+              className="border rounded-xl py-3 px-5 w-max hover:opacity-70"
+              type="submit"
+            >
+              Login
+            </button>
+          </div>
+        </form>
+      </div>
+    </main>
+  );
+};
+
+export default Login;
